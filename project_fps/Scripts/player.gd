ebd83bbcafbed24f9cf1ddbc0703af992b64e495
taromilkt <49883801+taromilkt@users.noreply.github.com>
2024-02-25 22:37:35 -0800

    Fixed camera jittering during Hold Crouch

--- project_fps/Scripts/player.gd
+++ project_fps/Scripts/player.gd
@@ -1,30 +1,33 @@
 extends CharacterBody3D
 
-# Gravity variables
-const JUMP_VELOCITY = 5.0
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ VARIABLES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Gravity
+const jump_velocity = 5.0
 var gravity = 9.8
 
-# Movement variables
-const SENSITIVITY = 0.0006
-const WALK_SPEED = 3.2
-const CROUCH_SPEED = 2.0
-const SPRINT_SPEED = 6.6
+# Movement
+const sensitivity = 0.0006
+const walk_speed = 3.2
+const crouch_speed = 2.0
+const sprint_speed = 6.3
 var speed
+var is_crouching = false
 
-# Bob variables
-const BOB_FREQ = 2.4
-const BOB_AMP = 0.023
+# Bob
+const bob_freq = 2.4
+const bob_amp = 0.023
 var t_bob = 0.0
 
-# FOV variables
-const BASE_FOV = 75.0
-const FOV_CHANGE = 0.75
+# FOV
+const base_fov = 75.0
+#const fov_change = 0.75
+const fov_change = 0.75
 
-# Paintball variables
+# Paintball
 var paintball = load("res://Models/marker/paintball.tscn")
 var instance
 
-# Define onready variables so we can use head and camera var later
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DEFINING ONREADY VARIABLES ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 @onready var head = $head
 @onready var camera = $head/Camera3D
 @onready var marker_anim = $head/Camera3D/marker/AnimationPlayer
@@ -33,8 +36,7 @@ var instance
 @onready var marker_smoke = $head/Camera3D/marker/GPUParticles3D
 @onready var crouch_anim = $AnimationPlayer
 
-
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 func _ready():
 	# Disable cursor 
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -44,9 +46,9 @@ func _ready():
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		# when mouse moves up/down which controls y angle, rotate about the x axis
-		head.rotate_y(-event.relative.x * SENSITIVITY)
+		head.rotate_y(-event.relative.x * sensitivity)
 		# when mouse moves left right which controls x angle, rotate about the y axis
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * sensitivity)
 
 		# limit camera y angle rotation by clamping camera.rotation.x b/n -55 and 55 degrees
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-55), deg_to_rad(55))
@@ -59,15 +61,15 @@ func _physics_process(delta):
 
 	# Jump Handling (cant jump when holding crouch)
 	if Input.is_action_just_pressed("jump") and is_on_floor() and !Input.is_action_pressed("crouch"):
-		velocity.y = JUMP_VELOCITY * 1.3
+		velocity.y = jump_velocity * 1.3
 
 	# Speed Handling
 	if Input.is_action_pressed("sprint"):
-		speed = SPRINT_SPEED
+		speed = sprint_speed
 	elif Input.is_action_pressed("crouch"):
-		speed = CROUCH_SPEED
+		speed = crouch_speed
 	else:
-		speed = WALK_SPEED
+		speed = walk_speed
 
 	# Movement Handling (Get input direction and handle the movement/deceleration)
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
@@ -95,14 +97,26 @@ func _physics_process(delta):
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
 
-	# FOV (only activate when sprint is held)
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	# FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, sprint_speed * 2)
+	var target_fov = base_fov + fov_change * velocity_clamped
+	# only activate when sprint is held
 	if Input.is_action_pressed("sprint"):
 		camera.fov = lerp(camera.fov, target_fov, delta * 2.0)
 	else:
-		camera.fov = lerp(camera.fov, BASE_FOV, delta * 6.0)
+		camera.fov = lerp(camera.fov, base_fov, delta * 4.0)
 
+	# Crouch Handling (dont allow crouch when sprinting)
+	# stay in crouch animation if ctrl is held down
+	if Input.is_action_pressed("crouch"):
+		if !is_crouching and is_on_floor() and !Input.is_action_pressed("sprint"):
+			crouch_anim.play("crouching")
+			is_crouching = true
+	# return to standing position if ctrl is let go
+	else:
+		if is_crouching:
+			crouch_anim.play_backwards("crouching")
+			is_crouching = false
 
 	# Firing
 	if Input.is_action_pressed("fire"):
@@ -116,7 +130,6 @@ func _physics_process(delta):
 			marker_smoke.emitting = true
 			marker_smoke.restart()
 			
-
 			# instantiate() creates a new object from the loaded paintball scene
 			instance = paintball.instantiate()
 			# set position of new paintball to global position of marker barrel raycast
@@ -126,26 +139,11 @@ func _physics_process(delta):
 			# parent is the world, add instance as child
 			get_parent().add_child(instance)
 
-
-	
-
-
-	# Crouch Handling (cant crouch when sprinting)
-	# stuck on how to play animation just once when crouch is held, SCREEN JITTERING
-	# MY ATTEMPT
-	if Input.is_action_pressed("crouch") and is_on_floor() and !Input.is_action_pressed("sprint"):
-		crouch_anim.play("crouching", 0.0, 1.0)
-	else:
-		crouch_anim.play("crouching", 0.0, -1.0)
-
-
-
-
 	move_and_slide()
 
 
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
-	pos.y = sin(time * BOB_FREQ) * BOB_AMP
-	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	pos.y = sin(time * bob_freq) * bob_amp
+	pos.x = cos(time * bob_freq / 2) * bob_amp
 	return pos
