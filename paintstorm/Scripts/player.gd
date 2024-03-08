5eef4a56a91b0e967acf00a42fcdb9d546022c07
taromilkt <49883801+taromilkt@users.noreply.github.com>
2024-03-08 14:45:46 -0800

    Updated player movement mechanics, fixed getting stuck on top of bunkers, disabled sprint during crouch or crawl, added esc input to exit while in game, corrected paintball marker angle to aim at center of screen, added crosshair

--- paintstorm/Scripts/player.gd
+++ paintstorm/Scripts/player.gd
@@ -1,19 +1,19 @@
 extends CharacterBody3D
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ VARIABLES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Gravity
-const jump_velocity = 5.0
+const sensitivity = 0.0005
 const gravity = 9.8
+const walk_speed = 3.0
+const crouch_speed = 1.4
+const crawl_speed = 1.0
+const sprint_speed = 5.5
+const jump_velocity = 6.8
+const head_height = 0.558
 
-# Movement
-const sensitivity = 0.0007
-const walk_speed = 3.3
-const crouch_speed = 2.0
-const prone_speed = 2.0
-const sprint_speed = 6.3
 var speed
-var is_crouching = false
-var is_prone = false
+var crouch_depth = -0.7
+var crawl_depth = -1.3
+var crawl_state = false
 
 # Bob
 const bob_freq = 2.4
@@ -35,70 +35,112 @@ var instance
 @onready var marker_barrel = $head/Camera3D/marker/RayCast3D
 @onready var marker_fire_audio = $head/Camera3D/marker/marker_fire
 @onready var marker_smoke = $head/Camera3D/marker/GPUParticles3D
-@onready var player_anim = $AnimationPlayer
+@onready var standing_collision_shape = $Standing_CollisionShape3D
+@onready var crouching_collision_shape = $Crouching_CollisionShape3D
+@onready var crawling_collision_shape = $Crawling_CollisionShape3D
+@onready var player_raycast = $RayCast3D
+@onready var player_shapecast = $ShapeCast3D
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 func _ready():
 	# Disable cursor 
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	# F3 to get debug stats
+	Engine.get_frames_per_second()
 
 
-# Camera Rotation
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		# when mouse moves up/down which controls y angle, rotate about the x axis
+		# when mouse up/down -> control y angle: rotate about x axis
 		head.rotate_y(-event.relative.x * sensitivity)
-		# when mouse moves left right which controls x angle, rotate about the y axis
+		# when mouse left/right -> control x angle: rotate about y axis
 		camera.rotate_x(-event.relative.y * sensitivity)
-
-		# limit camera y angle rotation by clamping camera.rotation.x b/n -55 and 55 degrees
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-55), deg_to_rad(30))
+		# limit y angle rotation by clamping camera.rotation.x
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(50))
 
 
 func _physics_process(delta):
-	# Add Gravity. Decrement from y velocity the falling speed due to gravity.
+	# Add Gravity
 	if not is_on_floor():
-		velocity.y -= gravity * delta * 2.0
+		velocity.y -= gravity * delta * 1.8
 
-	# Jump Handling (cant jump when holding crouch)
-	if Input.is_action_just_pressed("jump") and is_on_floor() and !Input.is_action_pressed("crouch") and !is_prone:
-		velocity.y = jump_velocity * 1.3
-
-	# Speed Handling
-	if Input.is_action_pressed("sprint"):
-		speed = sprint_speed
-	elif is_crouching:
-		speed = crouch_speed
-	elif is_prone:
-		speed = prone_speed
-	else:
-		speed = walk_speed
-
-	# Movement Handling (Get input direction and handle the movement/deceleration)
+	# Direction Handling
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
-	# Implement Intertia and Speed
-	# while on ground...	
-	if is_on_floor():
-		# how fast we move
-		if direction:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 8.0)
-			velocity.y = lerp(velocity.y, direction.y * speed, delta * 8.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 8.0)
-		# how fast we stop (using intertia)
+	# Jump Handling
+	if Input.is_action_just_pressed("jump") and is_on_floor() and !Input.is_action_pressed("crouch") and !Input.is_action_pressed("crawl") and !player_raycast.is_colliding():
+		velocity.y = jump_velocity
+
+	# Stance & Speed Handling --------------------------------------------------------
+	# Crawling State (Hold)
+	if Input.is_action_pressed("crawl") and is_on_floor():
+		speed = lerp(speed, crawl_speed, delta * 5.0)
+		if head.position.y > head_height + crouch_depth:
+			head.position.y = lerp(head.position.y, head_height + crawl_depth, delta * 1.4)
 		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 11.0)
+			head.position.y = move_toward(head.position.y, head_height + crawl_depth, delta * 1.8)
+
+		if head.position.y < head_height + crawl_depth + 0.3:
+			standing_collision_shape.disabled = true
+			crouching_collision_shape.disabled = true
+			crawling_collision_shape.disabled = false
+			crawl_state = true
+
+	# Crouching State (Hold)
+	elif Input.is_action_pressed("crouch") and is_on_floor() and !Input.is_action_pressed("crawl") and crawl_state == false:
+		speed = lerp(speed, crouch_speed, delta * 9.0)
+		head.position.y = lerp(head.position.y, head_height + crouch_depth, delta * 3.2)
+		if head.position.y < head_height + crouch_depth + 0.3:
+			standing_collision_shape.disabled = true
+			crouching_collision_shape.disabled = false
+			crawling_collision_shape.disabled = true
+
+	# Standing State
+	elif !player_shapecast.is_colliding():
+		if head.position.y < head_height + crouch_depth:
+			head.position.y = lerp(head.position.y, head_height, delta * 1.1)
+
+		else:
+			head.position.y = lerp(head.position.y, head_height, delta * 3.2)
+
+		standing_collision_shape.disabled = false
+		crouching_collision_shape.disabled = true
+		crawling_collision_shape.disabled = true
+		crawl_state = false
+
+		# Sprinting
+		if Input.is_action_pressed("sprint") and !Input.is_action_pressed("backward"):
+			speed = lerp(speed, sprint_speed, delta * 3.6)
+		# Walking
+		else:
+			if head.position.y < head_height + crouch_depth:
+				speed = move_toward(crawl_speed, crouch_speed, delta * 0.5)
+			else:
+				speed = walk_speed
+	#-------------------------------------------------------------------------------------
+	# Add Inertia
+	if is_on_floor():
+		# Starting Speed
+		if direction:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.y = lerp(velocity.y, direction.y * speed, delta * 6.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+		# Stopping Speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 8.0)
 			velocity.y = lerp(velocity.y, direction.y * speed, delta * 3.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 11.0)
-	# while in air (using intertia)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 8.0)
+	# In Air
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 1.5)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 1.5)
 
+
 	# Bob during movement
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
+
 
 	# FOV
 	var velocity_clamped = clamp(velocity.length(), 0.5, sprint_speed * 2)
@@ -108,50 +150,6 @@ func _physics_process(delta):
 		camera.fov = lerp(camera.fov, target_fov, delta * 2.0)
 	else:
 		camera.fov = lerp(camera.fov, base_fov, delta * 4.0)
-
-	# Crouch Handling (hold)
-	# stay in crouch animation if ctrl is held down
-	if Input.is_action_pressed("crouch"):
-		# player has to be on ground and not holding sprint
-		if !is_crouching and is_on_floor() and !Input.is_action_pressed("sprint") and !is_prone:
-			player_anim.play("crouching")
-			is_crouching = true
-	# return to standing position if ctrl is let go
-	else:
-		if is_crouching:
-			player_anim.play_backwards("crouching")
-			is_crouching = false
-
-	# Prone Handling (toggle)
-	# Need to fix animations
-	if Input.is_action_just_pressed("prone"):
-		# standing to prone
-		if !is_prone and is_on_floor() and !Input.is_action_pressed("crouch"):
-			player_anim.play("stand_to_prone")
-			is_prone = true
-		# prone to standing
-		elif is_prone and is_on_floor() and !Input.is_action_pressed("sprint") and !Input.is_action_pressed("crouch"):
-			player_anim.play_backwards("stand_to_prone")
-			is_prone = false
-		# prone to crouching
-		# elif is_prone and is_on_floor() and !Input.is_action_pressed("sprint") and Input.is_action_pressed("crouch"):
-		# 	player_anim.play_backwards("crouch_to_prone")
-		# 	is_prone = false
-
-		# when player is crouching
-		# if !is_prone and is_crouching:
-		# 	player_anim.play("crouch_to_prone")
-		# 	is_prone = true
-		# elif is_prone and is_crouching:
-		# 	player_anim.play_backwards("crouch_to_prone")
-		# 	is_prone = false
-		
-	# 	# when player not holding crouch in prone and press prone
-	# if is_prone and !is_crouching:
-	# 	if Input.is_action_just_pressed("prone"):
-	# 		player_anim.play_backwards("stand_to_prone")
-	# 		is_prone = false
-	
 
 
 	# Firing
@@ -179,6 +177,11 @@ func _physics_process(delta):
 			get_parent().add_child(instance)
 
 	move_and_slide()
+
+
+	# Exit Program
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
 
 
 func _headbob(time) -> Vector3:
